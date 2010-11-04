c6b34a42c1636a233296f5a43608be7a5ccbe858
gebba <johan.sjolin@gmail.com>
2010-11-04 17:42:35 +0100

    Added Settings class to handle settings loading and saving.

---
+++ Settings.h
@@ -0,0 +1,16 @@
+//
+//  Settings.h
+//  Minecraft Server
+//
+//  Created by Johan Sjölin on 11/4/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Settings : NSObject {
+	
+}
+
+@end
---
+++ Settings.m
@@ -0,0 +1,14 @@
+//
+//  Settings.m
+//  Minecraft Server
+//
+//  Created by Johan Sjölin on 11/4/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "Settings.h"
+
+
+@implementation Settings
+
+@end
